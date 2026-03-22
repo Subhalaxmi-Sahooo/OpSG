8b5ef13310c19c9830425db8d2fcba065d212fc3
Subhalaxmi-Sahooo <mtg6375@gmail.com>
2026-03-22 19:09:41 +0530

    Clean up tracked ROS2 workspace folders

--- build/my_drone_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/subhalaxmi/nidar_ws/src/my_drone_bringup
-# Build directory: /home/subhalaxmi/nidar_ws/build/my_drone_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/my_drone_bringup/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/subhalaxmi/nidar_ws/install/drone_pkg/share/drone_pkg/package.sh"
